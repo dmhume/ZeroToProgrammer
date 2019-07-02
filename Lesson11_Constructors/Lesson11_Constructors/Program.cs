dab782469e2b7fbdb0092c3a5d89c8a9c6ee6c4b
David Hume <dmhume@iu.edu>
2019-07-02 15:25:56 -0500

    Lesson 11

---
+++ Lesson11_Constructors/Lesson11_Constructors/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesson11_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Color = "Blue";
+
+            Car myCar2 = new Car();
+            myCar2.Color = "Red";
+
+            int total = Math.Add(5, 3);
+
+            Console.WriteLine(myCar.Color);
+            Console.WriteLine(myCar2.Color);
+            Console.WriteLine(total);
+        }
+    }
+
+    class Car
+    {
+        public string Color { get; set; }
+    }
+
+    class Math
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
