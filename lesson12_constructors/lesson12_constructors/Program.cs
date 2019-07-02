8c0083ddd7344882a15311f0367e961c5c7248ff
David Hume <dmhume@iu.edu>
2019-07-02 15:47:06 -0500

    Lesson 12-Constructors

---
+++ lesson12_constructors/lesson12_constructors/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lesson12_constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal myAnimal = new Animal();
+
+            myAnimal.Name = "David";
+            myAnimal.Breathe();
+        }
+
+        class Animal : IAnimal
+        {
+            public string Name { get; set; }
+            public string Color { get; set; }
+
+            public void Breathe()
+            {
+                Console.WriteLine("I'm Breathing");
+            }
+        }
+
+        class Dog : IAnimal
+        {
+            public string Name { get; set; }
+
+            public void Breathe()
+            {
+                Console.WriteLine("I'm Breathing");
+            }
+        }
+
+        interface IAnimal
+        {
+            string Name { get; set; }
+
+            void Breathe();
+
+        }
+    }
+}
